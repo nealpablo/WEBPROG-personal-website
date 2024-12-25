da87d276fd242375c45c9bdbeb264ef2bdc75201
nealpablo <idpablo@student.apc.edu.ph>
2024-12-25 22:41:44 +0800

    added another js function

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,12 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
+
+  function myFunction2() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+
+    const element = document.getElementsByTagName("p");
+
+    document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: <br>' + element[0].innerHTML;
+
+  }
