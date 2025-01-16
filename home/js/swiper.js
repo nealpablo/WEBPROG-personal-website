ed30a42bdbe1b459f6cfc3ca2dc333eb1e72fff8
nealpablo <idpablo@student.apc.edu.ph>
2025-01-16 14:44:30 +0000

    update last

--- home/js/swiper.js
+++ home/js/swiper.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var swiper = new Swiper('.swiper', {
-      loop: true,
-      pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-      },
-      navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-      },
-  });
+    var swiper = new Swiper('.mySwiper', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
 });
